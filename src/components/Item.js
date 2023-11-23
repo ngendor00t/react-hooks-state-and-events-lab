dc11b838dee8c6c31239427807ccb8b0bc5cdd87
Reginawambui <ngendoanita8@gmail.com>
2023-11-23 21:51:37 +0300

    displayed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-function Item({ name, category }) {
+function Item ({ name, category }) {
+
+
+
+const [inCart, setclassName] = useState(false)
+  
+function handleClick(){
+  setclassName(() => !inCart)
+}
+
   return (
-    <li className="">
+    <li className={ inCart ? "in-Cart" :""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className="add">{incart ?"Remove" : Add to cart}</button>
     </li>
   );
 }
